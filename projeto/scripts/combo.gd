2f384cf2522673766f2495c87910b940c4bc3f24
Tadeu1993 <113960787+Tadeu1993@users.noreply.github.com>
2025-02-21 02:32:13 -0300

    Delete projeto directory

--- projeto/scripts/combo.gd
+++
@@ -1,4 +0,0 @@
-extends Label
-
-func _on_anim_animation_finished(anim_name):
-	get_parent().queue_free()
--- projeto/scripts/main.gd
+++
@@ -1,4 +0,0 @@
-extends Node2D
-
-func _on_btnPlay_pressed():
-	loader.goto_scene("res://scenes/level.tscn")
